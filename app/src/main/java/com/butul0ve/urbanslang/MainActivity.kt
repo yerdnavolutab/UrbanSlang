4e5494cf311ccba1fac69f9744cad49e52cd1fc6
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-11-06 10:10:34 +0300

    added base views to MainActivity

--- app/src/main/java/com/butul0ve/urbanslang/MainActivity.kt
+++ app/src/main/java/com/butul0ve/urbanslang/MainActivity.kt
@@ -2,11 +2,34 @@ package com.butul0ve.urbanslang
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        TODO("hide the keyboard, add the fragment to the backstack and open it")
+        return false
+    }
+
+    private fun initUI() {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        navigationView.setNavigationItemSelectedListener(this)
+        toggle.syncState()
     }
 }
