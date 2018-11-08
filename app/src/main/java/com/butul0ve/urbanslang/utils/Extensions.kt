48c412b1cf5b6a7e4c51ff7a06f980a6b6432cbe
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-11-08 16:45:20 +0300

    created extension for reading from assets to the map

--- app/src/main/java/com/butul0ve/urbanslang/utils/Extensions.kt
+++ app/src/main/java/com/butul0ve/urbanslang/utils/Extensions.kt
@@ -1,7 +1,12 @@
 package com.butul0ve.urbanslang.utils
 
+import android.content.Context
 import android.support.v4.app.Fragment
 import com.butul0ve.urbanslang.mvp.main.MainFragment
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.IOException
+
 
 fun String.convertToFragment(): Fragment {
 
@@ -9,4 +14,26 @@ fun String.convertToFragment(): Fragment {
         MainFragment::class.java.simpleName -> MainFragment()
         else -> Fragment()
     }
+}
+
+@Throws(IOException::class)
+fun readDictionaryFromAssets(context: Context): Map<String, List<String>> {
+    val dictionary = HashMap<String, List<String>>()
+    val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+
+    alphabet.forEach {
+        val reader = BufferedReader(InputStreamReader(context.assets.open("$it.txt")))
+
+        val words = ArrayList<String>()
+
+        reader.lineSequence().forEach {
+            line -> words.add(line)
+        }
+
+        dictionary[it.toString()] = words
+
+        reader.close()
+    }
+
+    return dictionary
 }
