dc4ccf073220dd8d194d90055b5dd5e9cf7384f5
Andrey Batulov <a.batulov@eapteka.ru>
2023-10-22 22:36:12 +0300

    upgrade gradle version
    (and related libraries)

--- app/src/main/java/com/butul0ve/urbanslang/mvvm/factory/ViewModelFactory.kt
+++ app/src/main/java/com/butul0ve/urbanslang/mvvm/factory/ViewModelFactory.kt
@@ -10,7 +10,7 @@ class ViewModelFactory(
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainViewModel(dataManager) as T
     }
 }
